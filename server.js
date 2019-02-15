c7340a49cf3ac820df1ba9537d25c192eec7b4ee
Betty-Kebenei <kebeneibetty13@gmail.com>
2019-02-15 18:39:30 +0300

    add server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const serveStatic = require('serve-static');
 
 const app = express();
-app.use(serveStatic(path.join('/dist')));
+app.use(serveStatic(path.join('dist')));
 const port = process.env.PORT || 5000;
 app.listen(port);
 
